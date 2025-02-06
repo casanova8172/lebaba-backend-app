d8ca18a38cee86edff5ea7bf86ceb1273e039293
Sah <leelanandnitd@gmail.com>
2025-02-06 13:16:50 +0530

    url update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://lebaba-frontend-app-kqj1.vercel.app',
     credentials: true
 }))
 
